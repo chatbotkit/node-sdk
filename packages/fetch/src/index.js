4537bab04df23e32aba3abbd5e37a9b6e622e0e3
Petko D. Petkov <pdp@websecurify.com>
2023-06-12 19:47:58 +0000

    Refactored fetch.

--- packages/fetch/src/index.js
+++ packages/fetch/src/index.js
@@ -1,3 +1,22 @@
-import fetch, { Blob, FormData } from 'node-fetch'
+import { fetch, Blob, FormData } from 'node-fetch-native'
+
+if (
+  typeof globalThis.ReadableStream === 'function' &&
+  // @ts-ignore
+  typeof globalThis.ReadableStream.prototype[Symbol.asyncIterator] !==
+    'function'
+) {
+  // @ts-ignore
+  globalThis.ReadableStream.prototype[Symbol.asyncIterator] = function () {
+    const reader = this.getReader()
+
+    return {
+      next: () => reader.read(),
+      return: () => {
+        reader.releaseLock()
+      },
+    }
+  }
+}
 
 export { fetch, Blob, FormData }
