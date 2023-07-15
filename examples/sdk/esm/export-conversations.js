5e0daf82ab96c80676ae3e86e2651317ea7868c1
Petko D. Petkov <pdp@websecurify.com>
2023-07-15 10:59:31 +0000

    Created an export example.

---
+++ examples/sdk/esm/export-conversations.js
@@ -0,0 +1,52 @@
+import * as dotenv from 'dotenv'
+
+dotenv.config()
+
+import fs from 'fs/promises'
+import { ConversationClient } from '@chatbotkit/sdk/conversation/index.js'
+
+/**
+ * Retrieves conversations and their messages from the Chatbotkit Conversation
+ * API using the ChatbotKit SDK's stream() method. It exports the conversations
+ * to a JSON file named "conversations.json" with their respective messages.
+ */
+async function main() {
+  const client = new ConversationClient({
+    secret: process.env.CHATBOTKIT_API_SECRET,
+  })
+
+  const conversations = []
+
+  for await (const { data: conversationData } of client.list().stream()) {
+    console.log(`* exporting conversation ${conversationData.id}`)
+
+    const conversation = {
+      id: conversationData.id,
+
+      createdAt: conversationData.createdAt,
+
+      messages: [],
+    }
+
+    for await (const { data: messageData } of client.message
+      .list(conversationData.id)
+      .stream()) {
+      conversation.messages.push({
+        id: messageData.id,
+
+        type: messageData.type,
+        text: messageData.text,
+
+        createdAt: messageData.createdAt,
+      })
+    }
+
+    conversations.push(conversation)
+  }
+
+  const jsonContent = JSON.stringify(conversations, null, 2)
+
+  await fs.writeFile('conversations.json', jsonContent)
+}
+
+main()
