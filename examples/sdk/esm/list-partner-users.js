e1fbe6739f5ff0ebc337c3dbbd865f0c265e0120
pdparchitect <pdp@gnucitizen.org>
2023-11-06 07:47:39 +0000

    Added script to list partner users.

---
+++ examples/sdk/esm/list-partner-users.js
@@ -0,0 +1,25 @@
+import * as dotenv from 'dotenv'
+
+dotenv.config()
+
+import { UserClient } from '@chatbotkit/sdk/partner/user/index.js'
+
+async function main() {
+  if (!process.env.CHATBOTKIT_API_SECRET) {
+    console.log(`Please set the CHATBOTKIT_API_SECRET environment variable.`)
+
+    process.exit(1)
+  }
+
+  const client = new UserClient({
+    secret: process.env.CHATBOTKIT_API_SECRET,
+  })
+
+  for await (const { data } of client.list().stream()) {
+    console.log(`id: ${JSON.stringify(data.id)}`)
+    console.log(`name: ${JSON.stringify(data.name)}`)
+    console.log(`---`)
+  }
+}
+
+main()
