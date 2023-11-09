51f43a9f9197ec2f9cba3787a9cec1c15a5ae514
pdparchitect <pdp@gnucitizen.org>
2023-11-09 21:10:50 +0000

    Added example.

---
+++ examples/sdk/esm/create-dataset.js
@@ -0,0 +1,26 @@
+import * as dotenv from 'dotenv'
+
+dotenv.config()
+
+import { DatasetClient } from '@chatbotkit/sdk/dataset/index.js'
+
+async function main() {
+  const client = new DatasetClient({
+    secret: process.env.CHATBOTKIT_API_SECRET,
+  })
+
+  const { id } = await client.create({
+    name: 'random',
+    description: 'purely random dataset',
+    store: 'ada-loom'
+  })
+
+  console.log(`* dataset ${id} created`)
+
+  await client.delete(id)
+
+  console.log(`* dataset ${id} deleted`)
+}
+
+main()
+
