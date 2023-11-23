253be96148aad6732c654cc44fec4196391fa6f0
pdparchitect <pdp@gnucitizen.org>
2023-11-23 08:29:51 +0000

    Added streaming method.

---
+++ packages/next/src/stream.js
@@ -0,0 +1,34 @@
+/**
+ * @typedef {Generator<object>|AsyncGenerator<object>} IterableObject
+ *
+ * @param {IterableObject|{stream: () => IterableObject}} source
+ * @returns {Response}
+ */
+export function stream(source) {
+  const encoder = new TextEncoder()
+
+  return new Response(
+    new ReadableStream({
+      async start(controller) {
+        /** @type {IterableObject} */
+        let it
+
+        if ('next' in source && typeof source.next === 'function') {
+          it = source
+        } else if ('stream' in source && typeof source.stream === 'function') {
+          it = source.stream()
+        } else {
+          throw new Error('Invald source')
+        }
+
+        for await (const event of it) {
+          controller.enqueue(encoder.encode(`${JSON.stringify(event)}\n`))
+        }
+
+        controller.close()
+      },
+    })
+  )
+}
+
+export default stream
