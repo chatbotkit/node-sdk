80de1c1bcc2a39661f3ae0dcccb1b7751b5f9cad
pdparchitect <pdp@gnucitizen.org>
2024-02-11 00:42:25 +0000

    Added onFocus adjustments.

--- packages/react/src/components/AutoTextarea.js
+++ packages/react/src/components/AutoTextarea.js
@@ -2,19 +2,24 @@ import React from 'react'
 
 /**
  * @typedef {(event: React.ChangeEvent<HTMLTextAreaElement>) => any} onInputFn
+ * @typedef {(event: React.ChangeEvent<HTMLTextAreaElement>) => any} onFocusFn
+ *
+ * A textarea that automatically adjusts its height based on its content. The
+ * height is adjusted on input and focus events.
  *
  * @param {{
  *   onInput?: onInputFn?,
+ *   onFocus?: onFocusFn?,
  *   [name: string]: any
  * }} [props]
  */
 export function AutoTextarea(props) {
-  const { onInput, ...rest } = props || {}
+  const { onInput, onFocus, ...rest } = props || {}
 
   /**
    * @param {React.ChangeEvent<HTMLTextAreaElement>} event
    */
-  function handleOnInput(event) {
+  function handleEvent(event) {
     const adjustment = `calc(${
       [event.target.style.paddingTop, event.target.style.paddingBottom]
         .filter((f) => f)
@@ -23,13 +28,35 @@ export function AutoTextarea(props) {
 
     event.target.style.height = 'auto'
     event.target.style.height = `calc(${event.target.scrollHeight}px - ${adjustment})`
+  }
+
+  /**
+   * @param {React.ChangeEvent<HTMLTextAreaElement>} event
+   */
+  function handleOnInput(event) {
+    handleEvent(event)
 
     if (onInput) {
       onInput(event)
     }
   }
 
-  return <textarea {...rest} rows={1} onInput={handleOnInput} />
+  function handleOnFocus(event) {
+    handleEvent(event)
+
+    if (onFocus) {
+      onFocus(event)
+    }
+  }
+
+  return (
+    <textarea
+      {...rest}
+      rows={1}
+      onInput={handleOnInput}
+      onFocus={handleOnFocus}
+    />
+  )
 }
 
 export default AutoTextarea
