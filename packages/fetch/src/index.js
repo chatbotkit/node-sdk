1ab14d53ae58ddcb9c48dd996dae86d4afe34c61
pdparchitect <pdp@gnucitizen.org>
2025-06-19 08:53:02 +0000

    feat: enhance fetch functionality with improved error handling and status mapping

--- packages/fetch/src/index.js
+++ packages/fetch/src/index.js
@@ -1,41 +1,218 @@
-import { Blob, FormData, fetch } from 'node-fetch-native'
+import { Blob, FormData, fetch as nativeFetch } from 'node-fetch-native'
 
-export { fetch, Blob, FormData }
+export { Blob, FormData }
 
-if (
-  // eslint-disable-next-line no-undef
-  typeof globalThis.ReadableStream === 'function' &&
-  // @ts-expect-error polyfill
-  // eslint-disable-next-line no-undef
-  typeof globalThis.ReadableStream.prototype[Symbol.asyncIterator] !==
-    'function'
-) {
-  // @ts-expect-error polyfill
-  // eslint-disable-next-line no-undef
-  globalThis.ReadableStream.prototype[Symbol.asyncIterator] = function () {
-    const reader = this.getReader()
+// we need to polyfill the ReadableStream for chrome and Safari
+{
+  if (
+    // eslint-disable-next-line no-undef
+    typeof globalThis.ReadableStream === 'function' &&
+    // @ts-expect-error polyfill
+    // eslint-disable-next-line no-undef
+    typeof globalThis.ReadableStream.prototype[Symbol.asyncIterator] !==
+      'function'
+  ) {
+    // @ts-expect-error polyfill
+    // eslint-disable-next-line no-undef
+    globalThis.ReadableStream.prototype[Symbol.asyncIterator] = function () {
+      const reader = this.getReader()
 
-    return {
-      next: () => reader.read(),
-      return: () => {
-        reader.releaseLock()
-      },
+      return {
+        next: () => reader.read(),
+        return: () => {
+          reader.releaseLock()
+
+          return Promise.resolve({ done: true })
+        },
+      }
     }
   }
+}
+
+export const OK_STATUS = 200
+export const NOT_MODIFIED_STATUS = 304
+export const BAD_REQUEST_STATUS = 400
+export const NOT_AUTHENTICATED_STATUS = 401
+export const NO_SUBSCRIPTION_STATUS = 402
+export const NOT_AUTHORIZED_STATUS = 403
+export const NOT_FOUND_STATUS = 404
+export const METHOD_NOT_ALLOWED_STATUS = 405
+export const TIMEOUT_STATUS = 408
+export const CONFLICT_STATUS = 409
+export const UNPROCESSABLE_ENTITY_STATUS = 422
+export const TOO_MANY_REQUESTS_STATUS = 429
+export const LIMITS_REACHED_STATUS = 429
+export const INTERNAL_SERVER_ERROR_STATUS = 500
+export const NOT_IMPLEMENTED_STATUS = 501
+export const BAD_GATEWAY_STATUS = 502
+export const SERVICE_UNAVAILABLE_STATUS = 503
+export const GATEWAY_TIMEOUT_STATUS = 504
+
+export const OK_MESSAGE = 'OK'
+export const NOT_MODIFIED_MESSAGE = 'Not modified'
+export const BAD_REQUEST_MESSAGE = 'Bad request'
+export const NOT_AUTHENTICATED_MESSAGE = 'Not authenticated'
+export const NOT_AUTHORIZED_MESSAGE = 'Not authorized'
+export const NO_SUBSCRIPTION_MESSAGE = 'No subscription'
+export const NOT_FOUND_MESSAGE = 'Not found'
+export const METHOD_NOT_ALLOWED_MESSAGE = 'Method not allowed'
+export const TIMEOUT_MESSAGE = 'Timeout'
+export const CONFLICT_MESSAGE = 'Conflict'
+export const UNPROCESSABLE_ENTITY_STATUS_MESSAGE = 'Unprocessable entity'
+export const TOO_MANY_REQUESTS_MESSAGE = 'Too many requests'
+export const LIMITS_REACHED_MESSAGE = 'Limits reached'
+export const INTERNAL_SERVER_ERROR_MESSAGE = 'Internal server error'
+export const NOT_IMPLEMENTED_MESSAGE = 'Not implemented'
+export const BAD_GATEWAY_MESSAGE = 'Bad gateway'
+export const SERVICE_UNAVAILABLE_MESSAGE = 'Service unavailable'
+export const GATEWAY_TIMEOUT_MESSAGE = 'Gateway timeout'
+
+export const OK_CODE = 'OK'
+export const NOT_MODIFIED_CODE = 'NOT_MODIFIED'
+export const BAD_REQUEST_CODE = 'BAD_REQUEST'
+export const NOT_AUTHENTICATED_CODE = 'NOT_AUTHENTICATED'
+export const NOT_AUTHORIZED_CODE = 'NOT_AUTHORIZED'
+export const NO_SUBSCRIPTION_CODE = 'NO_SUBSCRIPTION'
+export const NOT_FOUND_CODE = 'NOT_FOUND'
+export const METHOD_NOT_ALLOWED_CODE = 'METHOD_NOT_ALLOWED'
+export const TIMEOUT_CODE = 'TIMEOUT'
+export const CONFLICT_CODE = 'CONFLICT'
+export const UNPROCESSABLE_ENTITY_CODE = 'UNPROCESSABLE_ENTITY'
+export const TOO_MANY_REQUESTS_CODE = 'TOO_MANY_REQUESTS'
+export const LIMITS_REACHED_CODE = 'LIMITS_REACHED'
+export const INTERNAL_SERVER_ERROR_CODE = 'INTERNAL_SERVER_ERROR'
+export const NOT_IMPLEMENTED_CODE = 'NOT_IMPLEMENTED'
+export const BAD_GATEWAY_CODE = 'BAD_GATEWAY'
+export const SERVICE_UNAVAILABLE_CODE = 'SERVICE_UNAVAILABLE'
+export const GATEWAY_TIMEOUT_CODE = 'GATEWAY_TIMEOUT'
+
+/**
+ * @type {Record<number,string>}
+ */
+export const statusToCodeMap = {
+  [OK_STATUS]: OK_CODE,
+  [NOT_MODIFIED_STATUS]: NOT_MODIFIED_CODE,
+  [BAD_REQUEST_STATUS]: BAD_REQUEST_CODE,
+  [NOT_AUTHENTICATED_STATUS]: NOT_AUTHENTICATED_CODE,
+  [NOT_AUTHORIZED_STATUS]: NOT_AUTHORIZED_CODE,
+  [NO_SUBSCRIPTION_STATUS]: NO_SUBSCRIPTION_CODE,
+  [NOT_FOUND_STATUS]: NOT_FOUND_CODE,
+  [METHOD_NOT_ALLOWED_STATUS]: METHOD_NOT_ALLOWED_CODE,
+  [TIMEOUT_STATUS]: TIMEOUT_CODE,
+  [CONFLICT_STATUS]: CONFLICT_CODE,
+  [UNPROCESSABLE_ENTITY_STATUS]: UNPROCESSABLE_ENTITY_CODE,
+  [TOO_MANY_REQUESTS_STATUS]: TOO_MANY_REQUESTS_CODE,
+  // [LIMITS_REACHED_STATUS]: LIMITS_REACHED_CODE, // disabled because duplicate status
+  [INTERNAL_SERVER_ERROR_STATUS]: INTERNAL_SERVER_ERROR_CODE,
+  [NOT_IMPLEMENTED_STATUS]: NOT_IMPLEMENTED_CODE,
+  [BAD_GATEWAY_STATUS]: BAD_GATEWAY_CODE,
+  [SERVICE_UNAVAILABLE_STATUS]: SERVICE_UNAVAILABLE_CODE,
+  [GATEWAY_TIMEOUT_STATUS]: GATEWAY_TIMEOUT_CODE,
+}
+
+export class FetchError extends Error {
+  /**
+   * @param {string} message
+   * @param {string} code
+   * @param {Record<string,any>} [meta]
+   */
+  constructor(message, code, meta) {
+    super(message)
+
+    this.name = meta
+      ? 'FetchError'
+      : `FetchError(${JSON.stringify(meta || {})})`
+    this.code = code || 'FetchError'
+  }
+}
+
+/**
+ * Create a FetchError from a Response object.
+ *
+ * @param {Response} response
+ * @param {Record<string,any>} [meta]
+ * @returns {Promise<FetchError>}
+ */
+export async function getFetchError(response, meta) {
+  if (response.ok) {
+    throw new Error(`Response ok: ${response.status} ${response.statusText}`)
+  }
+
+  const status = response.status
+  const text = await response.text()
+
+  let json
+
+  try {
+    json = JSON.parse(text)
+  } catch (e) {
+    json = { message: text, code: statusToCodeMap[status] }
+  }
+
+  return new FetchError(
+    json.message || text,
+    json.code || statusToCodeMap[status] || statusToCodeMap[500],
+    meta
+  )
 }
 
 export const ABORT_ERROR_NAME = 'AbortError'
 export const TIMEOUT_ERROR_NAME = 'TimeoutError'
 
+export const DEFAULT_TIMEOUT = process.env.FETCH_DEFAULT_TIMEOUT
+  ? parseInt(process.env.FETCH_DEFAULT_TIMEOUT, 10)
+  : 30000
+
+export const DEFAULT_RETRIES = 5
+export const DEFAULT_RETRY_DELAY = 250
+export const DEFAULT_RETRY_TIMEOUT = false
+export const DEFAULT_RETRY_STATUSES = [429, 500, 502, 503, 504]
+
+export class AbortError extends Error {
+  /**
+   * @param {string} [message]
+   */
+  constructor(message) {
+    super(message || ABORT_ERROR_NAME)
+
+    this.name = ABORT_ERROR_NAME
+    this.code = ABORT_ERROR_NAME
+  }
+}
+
+export class TimeoutError extends Error {
+  /**
+   * @param {string} [message]
+   */
+  constructor(message) {
+    super(message || TIMEOUT_ERROR_NAME)
+
+    this.name = TIMEOUT_ERROR_NAME
+    this.code = TIMEOUT_ERROR_NAME
+  }
+}
+
 /**
- * @internal
- * @param {AbortSignal[]} signals
+ * @param {string|URL} url
+ * @param {RequestInit} [init]
+ * @returns {Promise<Response>}
+ */
+export function fetch(url, init) {
+  return nativeFetch(url, init)
+}
+
+/**
+ * @param {(AbortSignal|null|undefined)[]} signals
  * @returns {AbortSignal}
  */
-function anySignal(signals) {
+export function anySignal(signals) {
   const controller = new AbortController()
 
   for (const signal of signals) {
+    if (!signal) {
+      continue
+    }
+
     if (signal.aborted) {
       controller.abort(signal.reason)
 
@@ -51,28 +228,8 @@ function anySignal(signals) {
 }
 
 /**
- */
-export class FetchError extends Error {
-  /**
-   * @param {string} message
-   * @param {number|string} code
-   * @param {string} url
-   * @param {RequestInit & withTimeoutOptions & withRetryOptions} request
-   * @param {Response} response
-   */
-  constructor(message, code, url, request, response) {
-    super(message)
-
-    this.code = code || response.status
-
-    this.url = url
-    this.request = request
-    this.response = response
-  }
-}
-
-/**
- * @typedef {(url: string, options?: RequestInit) => Promise<Response>} FetchFn
+ * @template T
+ * @typedef {(url: string|URL, options?: RequestInit & T) => Promise<Response>} FetchFn
  */
 
 /**
@@ -82,18 +239,19 @@ export class FetchError extends Error {
  *
  * Add timeout capabilities to any fetch implementation.
  *
- * @param {FetchFn} fetch
+ * @param {FetchFn<{}>} fetch
  * @param {withTimeoutOptions} [defaultOptions]
- * @returns {FetchFn}
+ * @returns {FetchFn<withTimeoutOptions>}
  */
 export function withTimeout(fetch, defaultOptions) {
   /**
-   * @param {string} url
+   * @param {string|URL} url
    * @param {RequestInit & withTimeoutOptions} [options]
    * @returns {Promise<Response>}
    */
   return async function fetchWithTimeout(url, options) {
-    const timeout = options?.timeout ?? defaultOptions?.timeout ?? 30000
+    const timeout =
+      options?.timeout ?? defaultOptions?.timeout ?? DEFAULT_TIMEOUT
 
     let signal
     let handler
@@ -109,7 +267,7 @@ export function withTimeout(fetch, defaultOptions) {
       handler = setTimeout(() => {
         isTimeOutAbort = true
 
-        abortController.abort(TIMEOUT_ERROR_NAME)
+        abortController.abort(new TimeoutError())
       }, timeout)
 
       // @todo use AbortSignal.any([]) when widely supported, right now most
@@ -132,13 +290,13 @@ export function withTimeout(fetch, defaultOptions) {
       })
     } catch (error) {
       // @note we have a problem because some implementation (Chrome) do not
-      // corretly transfer the real reason for the abort i.e. the timeout error,
-      // so we need to check if we have raised a timeout above and if so we need
-      // to throw the correct error
+      // correctly transfer the real reason for the abort i.e. the timeout
+      // error, so we need to check if we have raised a timeout above and if so
+      // we need to throw the correct error
 
       if ([error.name, error.message].includes(ABORT_ERROR_NAME)) {
         if (isTimeOutAbort) {
-          throw new Error(TIMEOUT_ERROR_NAME)
+          throw new TimeoutError()
         }
       }
 
@@ -159,23 +317,34 @@ export function withTimeout(fetch, defaultOptions) {
  *   retryStatuses?: number[]
  * }} withRetryOptions
  *
- * @param {FetchFn} fetch
+ * Add retry capabilities to any fetch implementation.
+ *
+ * @param {FetchFn<{}>} fetch
  * @param {withRetryOptions} [defaultOptions]
- * @returns {FetchFn}
+ * @returns {FetchFn<withRetryOptions>}
  */
 export function withRetry(fetch, defaultOptions) {
   /**
-   * @param {string} url
+   * @param {string|URL} url
    * @param {RequestInit & withRetryOptions} [options]
    * @returns {Promise<Response>}
    */
   return async function fetchWithRetry(url, options) {
-    const retries = options?.retries ?? defaultOptions?.retries ?? 5
-    const retryDelay = options?.retryDelay ?? defaultOptions?.retryDelay ?? 250
+    const retries =
+      options?.retries ?? defaultOptions?.retries ?? DEFAULT_RETRIES
+
+    const retryDelay =
+      options?.retryDelay ?? defaultOptions?.retryDelay ?? DEFAULT_RETRY_DELAY
+
     const retryTimeout =
-      options?.retryTimeout ?? defaultOptions?.retryTimeout ?? false
-    const retryStatuses = options?.retryStatuses ??
-      defaultOptions?.retryStatuses ?? [429, 500, 502, 503, 504]
+      options?.retryTimeout ??
+      defaultOptions?.retryTimeout ??
+      DEFAULT_RETRY_TIMEOUT
+
+    const retryStatuses =
+      options?.retryStatuses ??
+      defaultOptions?.retryStatuses ??
+      DEFAULT_RETRY_STATUSES
 
     let response
 
@@ -184,13 +353,10 @@ export function withRetry(fetch, defaultOptions) {
 
       if (!response.ok) {
         if (retryStatuses.includes(response.status)) {
-          throw new FetchError(
-            `Request failed`,
-            response.status,
-            url,
-            options || {},
-            response
-          )
+          throw await getFetchError(response, {
+            url: new URL(url).href,
+            options: options,
+          })
         } else {
           return response
         }
@@ -262,3 +428,5 @@ export async function* jsonl(body) {
     }
   }
 }
+
+export default fetch
--- packages/fetch/types/index.d.ts
+++ packages/fetch/types/index.d.ts
@@ -1,5 +1,25 @@
 /**
- * @typedef {(url: string, options?: RequestInit) => Promise<Response>} FetchFn
+ * Create a FetchError from a Response object.
+ *
+ * @param {Response} response
+ * @param {Record<string,any>} [meta]
+ * @returns {Promise<FetchError>}
+ */
+export function getFetchError(response: Response, meta?: Record<string, any> | undefined): Promise<FetchError>;
+/**
+ * @param {string|URL} url
+ * @param {RequestInit} [init]
+ * @returns {Promise<Response>}
+ */
+export function fetch(url: string | URL, init?: RequestInit | undefined): Promise<Response>;
+/**
+ * @param {(AbortSignal|null|undefined)[]} signals
+ * @returns {AbortSignal}
+ */
+export function anySignal(signals: (AbortSignal | null | undefined)[]): AbortSignal;
+/**
+ * @template T
+ * @typedef {(url: string|URL, options?: RequestInit & T) => Promise<Response>} FetchFn
  */
 /**
  * @typedef {{
@@ -8,11 +28,11 @@
  *
  * Add timeout capabilities to any fetch implementation.
  *
- * @param {FetchFn} fetch
+ * @param {FetchFn<{}>} fetch
  * @param {withTimeoutOptions} [defaultOptions]
- * @returns {FetchFn}
+ * @returns {FetchFn<withTimeoutOptions>}
  */
-export function withTimeout(fetch: FetchFn, defaultOptions?: withTimeoutOptions | undefined): FetchFn;
+export function withTimeout(fetch: FetchFn<{}>, defaultOptions?: withTimeoutOptions | undefined): FetchFn<withTimeoutOptions>;
 /**
  * @typedef {{
  *   retries?: number,
@@ -21,11 +41,13 @@ export function withTimeout(fetch: FetchFn, defaultOptions?: withTimeoutOptions 
  *   retryStatuses?: number[]
  * }} withRetryOptions
  *
- * @param {FetchFn} fetch
+ * Add retry capabilities to any fetch implementation.
+ *
+ * @param {FetchFn<{}>} fetch
  * @param {withRetryOptions} [defaultOptions]
- * @returns {FetchFn}
+ * @returns {FetchFn<withRetryOptions>}
  */
-export function withRetry(fetch: FetchFn, defaultOptions?: withRetryOptions | undefined): FetchFn;
+export function withRetry(fetch: FetchFn<{}>, defaultOptions?: withRetryOptions | undefined): FetchFn<withRetryOptions>;
 /**
  * Expose a JSONL stream as an async iterable.
  *
@@ -35,38 +57,103 @@ export function withRetry(fetch: FetchFn, defaultOptions?: withRetryOptions | un
 export function jsonl(body: ReadableStream<Uint8Array> & {
     [Symbol.asyncIterator](): AsyncIterator<Uint8Array>;
 }): AsyncGenerator<Record<string, any>>;
-export const ABORT_ERROR_NAME: "AbortError";
-export const TIMEOUT_ERROR_NAME: "TimeoutError";
+export const OK_STATUS: 200;
+export const NOT_MODIFIED_STATUS: 304;
+export const BAD_REQUEST_STATUS: 400;
+export const NOT_AUTHENTICATED_STATUS: 401;
+export const NO_SUBSCRIPTION_STATUS: 402;
+export const NOT_AUTHORIZED_STATUS: 403;
+export const NOT_FOUND_STATUS: 404;
+export const METHOD_NOT_ALLOWED_STATUS: 405;
+export const TIMEOUT_STATUS: 408;
+export const CONFLICT_STATUS: 409;
+export const UNPROCESSABLE_ENTITY_STATUS: 422;
+export const TOO_MANY_REQUESTS_STATUS: 429;
+export const LIMITS_REACHED_STATUS: 429;
+export const INTERNAL_SERVER_ERROR_STATUS: 500;
+export const NOT_IMPLEMENTED_STATUS: 501;
+export const BAD_GATEWAY_STATUS: 502;
+export const SERVICE_UNAVAILABLE_STATUS: 503;
+export const GATEWAY_TIMEOUT_STATUS: 504;
+export const OK_MESSAGE: "OK";
+export const NOT_MODIFIED_MESSAGE: "Not modified";
+export const BAD_REQUEST_MESSAGE: "Bad request";
+export const NOT_AUTHENTICATED_MESSAGE: "Not authenticated";
+export const NOT_AUTHORIZED_MESSAGE: "Not authorized";
+export const NO_SUBSCRIPTION_MESSAGE: "No subscription";
+export const NOT_FOUND_MESSAGE: "Not found";
+export const METHOD_NOT_ALLOWED_MESSAGE: "Method not allowed";
+export const TIMEOUT_MESSAGE: "Timeout";
+export const CONFLICT_MESSAGE: "Conflict";
+export const UNPROCESSABLE_ENTITY_STATUS_MESSAGE: "Unprocessable entity";
+export const TOO_MANY_REQUESTS_MESSAGE: "Too many requests";
+export const LIMITS_REACHED_MESSAGE: "Limits reached";
+export const INTERNAL_SERVER_ERROR_MESSAGE: "Internal server error";
+export const NOT_IMPLEMENTED_MESSAGE: "Not implemented";
+export const BAD_GATEWAY_MESSAGE: "Bad gateway";
+export const SERVICE_UNAVAILABLE_MESSAGE: "Service unavailable";
+export const GATEWAY_TIMEOUT_MESSAGE: "Gateway timeout";
+export const OK_CODE: "OK";
+export const NOT_MODIFIED_CODE: "NOT_MODIFIED";
+export const BAD_REQUEST_CODE: "BAD_REQUEST";
+export const NOT_AUTHENTICATED_CODE: "NOT_AUTHENTICATED";
+export const NOT_AUTHORIZED_CODE: "NOT_AUTHORIZED";
+export const NO_SUBSCRIPTION_CODE: "NO_SUBSCRIPTION";
+export const NOT_FOUND_CODE: "NOT_FOUND";
+export const METHOD_NOT_ALLOWED_CODE: "METHOD_NOT_ALLOWED";
+export const TIMEOUT_CODE: "TIMEOUT";
+export const CONFLICT_CODE: "CONFLICT";
+export const UNPROCESSABLE_ENTITY_CODE: "UNPROCESSABLE_ENTITY";
+export const TOO_MANY_REQUESTS_CODE: "TOO_MANY_REQUESTS";
+export const LIMITS_REACHED_CODE: "LIMITS_REACHED";
+export const INTERNAL_SERVER_ERROR_CODE: "INTERNAL_SERVER_ERROR";
+export const NOT_IMPLEMENTED_CODE: "NOT_IMPLEMENTED";
+export const BAD_GATEWAY_CODE: "BAD_GATEWAY";
+export const SERVICE_UNAVAILABLE_CODE: "SERVICE_UNAVAILABLE";
+export const GATEWAY_TIMEOUT_CODE: "GATEWAY_TIMEOUT";
 /**
+ * @type {Record<number,string>}
  */
+export const statusToCodeMap: Record<number, string>;
 export class FetchError extends Error {
     /**
      * @param {string} message
-     * @param {number|string} code
-     * @param {string} url
-     * @param {RequestInit & withTimeoutOptions & withRetryOptions} request
-     * @param {Response} response
+     * @param {string} code
+     * @param {Record<string,any>} [meta]
      */
-    constructor(message: string, code: number | string, url: string, request: RequestInit & withTimeoutOptions & withRetryOptions, response: Response);
-    code: string | number;
-    url: string;
-    request: RequestInit & withTimeoutOptions & withRetryOptions;
-    response: Response;
+    constructor(message: string, code: string, meta?: Record<string, any> | undefined);
+    code: string;
 }
-export type FetchFn = (url: string, options?: RequestInit) => Promise<Response>;
+export const ABORT_ERROR_NAME: "AbortError";
+export const TIMEOUT_ERROR_NAME: "TimeoutError";
+export const DEFAULT_TIMEOUT: number;
+export const DEFAULT_RETRIES: 5;
+export const DEFAULT_RETRY_DELAY: 250;
+export const DEFAULT_RETRY_TIMEOUT: false;
+export const DEFAULT_RETRY_STATUSES: number[];
+export class AbortError extends Error {
+    code: string;
+}
+export class TimeoutError extends Error {
+    code: string;
+}
+export default fetch;
+export type FetchFn<T> = (url: string | URL, options?: RequestInit & T) => Promise<Response>;
 /**
  * Add timeout capabilities to any fetch implementation.
  */
 export type withTimeoutOptions = {
     timeout?: number;
 };
+/**
+ * Add retry capabilities to any fetch implementation.
+ */
 export type withRetryOptions = {
     retries?: number;
     retryDelay?: number;
     retryTimeout?: boolean;
     retryStatuses?: number[];
 };
-import { fetch } from 'node-fetch-native';
 import { Blob } from 'node-fetch-native';
 import { FormData } from 'node-fetch-native';
-export { fetch, Blob, FormData };
+export { Blob, FormData };
