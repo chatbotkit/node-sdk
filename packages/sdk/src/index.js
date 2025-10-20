1a39a363bb052778c3669c3ee23b888b7852f502
pdparchitect <pdp@gnucitizen.org>
2025-10-20 00:31:24 +0000

    feat: add MemoryClient and TeamClient imports and initialization in ChatBotKit

--- packages/sdk/src/index.js
+++ packages/sdk/src/index.js
@@ -8,11 +8,13 @@ import { FileClient } from './file/index.js'
 import { GraphqlClient } from './graphql/index.js'
 import { IntegrationClient } from './integration/index.js'
 import { MagicClient } from './magic/index.js'
+import { MemoryClient } from './memory/index.js'
 import { PartnerClient } from './partner/index.js'
 import { PlatformClient } from './platform/index.js'
 import { SecretClient } from './secret/index.js'
 import { SkillsetClient } from './skillset/index.js'
 import { TaskClient } from './task/index.js'
+import { TeamClient } from './team/index.js'
 
 export { BotClient }
 export { FileClient }
@@ -26,6 +28,8 @@ export { ContactClient }
 export { SecretClient }
 export { BlueprintClient }
 export { GraphqlClient }
+export { MemoryClient }
+export { TeamClient }
 
 export class ChatBotKit extends ChatBotKitClient {
   /**
@@ -46,6 +50,8 @@ export class ChatBotKit extends ChatBotKitClient {
     this.conversation = new ConversationClient(options)
     this.contact = new ContactClient(options)
     this.task = new TaskClient(options)
+    this.memory = new MemoryClient(options)
+    this.team = new TeamClient(options)
 
     // @note overlapping name with the `secret` property
     this.secrets = new SecretClient(options)
