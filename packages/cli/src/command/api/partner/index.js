41895456d26c6df682c66b1d1a21d4dc1e7b128d
pdparchitect <pdp@gnucitizen.org>
2025-11-13 09:22:30 +0000

    refactor: streamline command registration for dataset, partner, and skillset APIs

--- packages/cli/src/command/api/partner/index.js
+++ packages/cli/src/command/api/partner/index.js
@@ -1,10 +1,27 @@
 import user from './user/index.js'
 
+import { PartnerClient } from '@chatbotkit/sdk/partner/index.js'
+
 import { Command } from 'commander'
+
+/**
+ * Commands registry - MUST include ALL PartnerClient methods
+ *
+ * @todo enable types once we have more SDK clients implemented
+ *
+ * _satisfies {Partial<Record<keyof PartnerClient, import('commander').Command>>}
+ * _type {Record<keyof PartnerClient, import('commander').Command>}
+ */
+const commands = {
+  user,
+}
 
 export const command = new Command()
   .name('partner')
   .description('Partner tools for ChatBotKit')
-  .addCommand(user)
+
+for (const cmd of Object.values(commands)) {
+  command.addCommand(cmd)
+}
 
 export default command
