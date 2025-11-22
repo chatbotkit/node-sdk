ac72243721ce17d922fe60f42160f42c6aaa3823
pdparchitect <pdp@gnucitizen.org>
2025-11-22 10:54:20 +0000

    feat: add bootstrap script for ChatBotKit integration in Next.js examples

---
+++ examples/vite/react/stateless-chat/bootstrap.js
@@ -0,0 +1,25 @@
+const fs = require('fs')
+const cp = require('child_process')
+const packageJson = require('./package.json')
+
+for (const dep in packageJson.dependencies) {
+  if (dep.startsWith('@chatbotkit/')) {
+    packageJson.dependencies[dep] = 'latest'
+  }
+}
+
+fs.writeFileSync(
+  './package.json',
+  JSON.stringify(packageJson, null, 2),
+  'utf-8'
+)
+
+fs.copyFileSync('./.env.example', './.env')
+
+cp.execSync('npm install', { stdio: 'inherit' })
+
+console.log('\nSetup complete!\n')
+console.log('Next steps:')
+console.log('   1. Edit .env file with your ChatBotKit credentials')
+console.log('   2. Run: npm run dev')
+console.log('\nHappy coding!\n')
