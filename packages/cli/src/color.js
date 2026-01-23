44667aab3c2676f32a0a8c2d4e937f5baa496f16
pdparchitect <pdp@gnucitizen.org>
2026-01-23 00:24:21 +0000

    feat: add color formatting functions for red and yellow text

--- packages/cli/src/color.js
+++ packages/cli/src/color.js
@@ -1,4 +1,6 @@
 export const BLUE = '\u001b[34m'
+export const RED = '\u001b[31m'
+export const YELLOW = '\u001b[33m'
 export const RESET = '\u001b[0m'
 
 /**
@@ -6,4 +8,18 @@ export const RESET = '\u001b[0m'
  */
 export const formatBlue = (text) => {
   return `${BLUE}${text}${RESET}`
+}
+
+/**
+ * @param {string} text
+ */
+export const formatRed = (text) => {
+  return `${RED}${text}${RESET}`
+}
+
+/**
+ * @param {string} text
+ */
+export const formatYellow = (text) => {
+  return `${YELLOW}${text}${RESET}`
 }
